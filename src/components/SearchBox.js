851aab070dea752748010cd19f7890a7ce2a654a
horusspade <eshane.j@gmail.com>
2018-11-07 19:20:23 -0500

    recommit

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -3,7 +3,8 @@ import React from 'react';
 const SearchBox = ({ searchfield, searchChange }) => {
   return (
     <div className='pa2'>
-      <input
+          <input
+              aria-label = 'Search Robots'
         className='pa3 ba b--green bg-lightest-blue'
         type='search'
         placeholder='search robots'
